5690a83b06168b2cd271dc95b526d630c016c353
Scott Pantall <aspantall@gmail.com>
2014-06-15 03:52:35 -0600

    Modifies Articles to add excerpt and location

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140615083933) do
+ActiveRecord::Schema.define(version: 20140615093851) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20140615083933) do
     t.datetime "published_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "excerpt"
+    t.string   "location"
   end
 
 end
